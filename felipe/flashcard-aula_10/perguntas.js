51b463d3ab010664527a211a232988361906e4ba
felipeschiavo <161044422+felipeschiavo@users.noreply.github.com>
2024-11-01 10:45:00 -0300

    Add files via upload

---
+++ felipe/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é variável em programação?',
+    'Epaço de armazenamento para dados'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o o rio mais extenso do mundo?',
+    'Rio Nilo'
+)
+
+criaCartao(
+    'Programação',
+    'Qual diferença entre "If" e "Switch"?',
+    '"If" para condições booleanas,"Switch" para selecionar opções'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a forma correta de dizer"eu estou muito feliz" em inglês?',
+    'I am extremely happy)'
+)
